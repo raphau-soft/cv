ef1968e95102376bf621cdcbd2444cf8ba43b2be
Rafał Piszko <piszkorafal@gmail.com>
2025-02-14 16:29:59 +0100

    init config

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition > windowHeight / 2) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
+}
