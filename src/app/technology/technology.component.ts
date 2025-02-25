44fc7aee6f25bd1f22660354867892ba88b88b70
Rafał Piszko <piszkorafal@gmail.com>
2025-02-25 20:53:09 +0100

    svg icons fix

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -11,19 +11,19 @@ import { HoverEffectDirective } from '../hover-effect.directive';
 })
 export class TechnologyComponent {
   technologies = [
-    { name: 'Java', icon: '/java.svg', url: 'https://www.oracle.com/java/' },
+    { name: 'Java', icon: 'java.svg', url: 'https://www.oracle.com/java/' },
     {
       name: 'Spring Boot',
-      icon: '/spring.svg',
+      icon: 'spring.svg',
       url: 'https://spring.io/projects/spring-boot',
     },
-    { name: 'Angular', icon: '/angular.svg', url: 'https://angular.io/' },
+    { name: 'Angular', icon: 'angular.svg', url: 'https://angular.io/' },
     {
       name: 'PostgreSQL',
-      icon: '/postgresql.svg',
+      icon: 'postgresql.svg',
       url: 'https://www.postgresql.org/',
     },
-    { name: 'Docker', icon: '/docker.svg', url: 'https://www.docker.com/' },
-    { name: 'Jenkins', icon: '/jenkins.svg', url: 'https://www.jenkins.io/' },
+    { name: 'Docker', icon: 'docker.svg', url: 'https://www.docker.com/' },
+    { name: 'Jenkins', icon: 'jenkins.svg', url: 'https://www.jenkins.io/' },
   ];
 }
