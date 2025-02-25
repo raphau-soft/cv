b4891d0ce0241c220ec36847601e0cc909a83362
Rafał Piszko <piszkorafal@gmail.com>
2025-02-25 18:25:53 +0100

    adjustment of books & courses section and open cv button]

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -13,17 +13,27 @@ export class BooksComponent {
       title: 'Web Application Security (Bezpieczeństwo aplikacji webowych)',
       author: 'Sekurak',
       year: 1999,
+      type: 'book',
+      link: 'https://sklep.securitum.pl/ksiazka-bezpieczenstwo-aplikacji-webowych',
     },
     {
       title: 'Microservices Patterns: With Examples in Java',
       author: 'Chris Richardson',
       year: 2018,
+      type: 'book',
+      link: 'https://microservices.io/book',
     },
     {
       title:
         'TDD, Clean Code, and Refactoring (JAVA, C#, Object-Oriented Languages)',
       author: 'Piotr Walczak',
       year: 2022,
+      type: 'course',
+      link: 'tdd_clean_code_and_refactoring_certificate.pdf',
     },
   ];
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
 }
