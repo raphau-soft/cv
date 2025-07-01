aa4ca308adc342af594b4bd8b4507be817876075
Rafał Piszko <piszkorafal@gmail.com>
2025-07-01 16:19:10 +0200

    technologies update

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -25,5 +25,11 @@ export class TechnologyComponent {
     },
     { name: 'Docker', icon: 'docker.svg', url: 'https://www.docker.com/' },
     { name: 'Jenkins', icon: 'jenkins.svg', url: 'https://www.jenkins.io/' },
+    { name: 'Elasticsearch', icon: 'elasticsearch.svg', url: 'https://www.elastic.co/elasticsearch' },
+    { name: 'Git', icon: 'git.svg', url: 'https://git-scm.com/' },
+    { name: 'Apache Kafka', icon: 'kafka.svg', url: 'https://kafka.apache.org/' },
+    { name: 'RabbitMQ', icon: 'rabbitmq.svg', url: 'https://www.rabbitmq.com/' },
+    { name: 'RobotFramework', icon: 'robotframework.svg', url: 'https://robotframework.org/' },
+    { name: 'Linux', icon: 'linux.svg', url: 'https://ubuntu.com/' },
   ];
 }
